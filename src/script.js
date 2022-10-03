881bd703f6af4660fd4c7bad8aa4952796a9cda7
Dima <example@gmail.com>
2022-10-03 16:06:51 +0300

    first commit

--- src/script.js
+++ src/script.js
@@ -33,6 +33,28 @@ const personalMovieDB = {
     privat: false
 };
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+    personalMovieDB[a] = b;
+    personalMovieDB[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+/* const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
 const a = prompt('Один из последний просмотренных фильмов?', ''),
       b = prompt('На сколько оцените его?', ''),
       c = prompt('Один из последний просмотренных фильмов?', ''),
@@ -41,4 +63,4 @@ const a = prompt('Один из последний просмотренных ф
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+console.log(personalMovieDB); */
